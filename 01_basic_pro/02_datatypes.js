90e061fa3578919b6bfe72cb5d8fd9b2b341f643
Jalpa Vadgama <jalpavadgama52@gmail.com>
2024-12-05 12:32:39 +0530

    datatype

---
+++ 01_basic_pro/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3 + 3); we r using nodejs, not browser
+console.log(3
+    +3);    // code readability should be heigh
+console.log("Jalpa");
+
+let name = "Jalpa";
+let age = 25;
+let isLoggedIn = false;
+
+// let state; // undefine
+// let state = null; // null
+// DataType
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefine =>
+// symbol => unique    
+// object
+
+console.log(typeof name);
+
+console.log(typeof null);
+console.log(typeof undefined);
