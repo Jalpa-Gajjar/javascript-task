c1a54e3de76d3903bdf32070b978bf05957e24d3
Jalpa Vadgama <jalpavadgama52@gmail.com>
2024-12-12 13:39:05 +0530

    conversion

---
+++ 01_basic_pro/03_conversiononOperation.js
@@ -0,0 +1,39 @@
+// Number Conversion
+
+// let score = 33;
+// let score = "33"; // 33
+// let score = "33abc" //NaN
+// let score = null; // 0
+let score = undefined; // NaN
+// let score = true; // 1
+// let score = false; // 0
+// let score = "Jalpa"; //NaN
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// Boolean Conversion   1=>true, 0=>false
+
+// let isLoggedIn = true;   //1
+// let isLoggedIn = false;  //0
+// let isLoggedIn = 1;  //true
+// let isLoggedIn = 0;  //false
+// let isLoggedIn = ""; //false
+// let isLoggedIn = "Jalpa";    // true
+let isLoggedIn = null;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+//String conversion 
+
+let someNumber = 33;
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
