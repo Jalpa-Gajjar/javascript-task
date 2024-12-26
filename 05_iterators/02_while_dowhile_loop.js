bfc4cd38bf76ac48f0d543f956e06ae82c5e3bc4
Jalpa Vadgama <jalpavadgama52@gmail.com>
2024-12-26 08:56:06 +0530

    while-dowhile loop

---
+++ 05_iterators/02_while_dowhile_loop.js
@@ -0,0 +1,31 @@
+// while loop
+/*
+let i = 0;
+
+while (i <= 10) {
+    console.log(`value of index is ${i}`);
+    i = i+2;    
+}
+*/
+
+// while loop on array
+/*
+let myArray = ["flash", "batsman", "superman"];
+let arr = 0;
+
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1;  // arr++; both r same
+}
+*/
+
+// do-while loop
+/*
+let score = 0;
+// let score = 11;
+do {
+    console.log(`score is ${score}`);
+    score++;    
+} while (score <= 10);
+*/
+
