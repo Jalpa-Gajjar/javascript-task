eca8aaf22be9a3ff75730f6685dcb5bfcad60277
Jalpa Vadgama <jalpavadgama52@gmail.com>
2025-01-21 11:56:53 +0530

    math function

---
+++ 10_classes_and_oop/09_mathpi.js
@@ -0,0 +1,38 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+// const mynewObject = Object.create(null) // object create through create word
+// object creation
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+// console.log(chai);
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
+    
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
